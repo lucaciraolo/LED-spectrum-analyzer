ec3d6ca852123fe7fd0c5039652cbcb698629c60
Luca Ciraolo <lucaciraolo@gmail.com>
2015-02-26 07:38:10 -0800

    Think I got it working

--- Arduino_Sketch/Arduino_Sketch.ino
+++ Arduino_Sketch/Arduino_Sketch.ino
@@ -1,19 +1,58 @@
 #include <Adafruit_NeoPixel.h>
 
-int COLUMNS_ENABLED = 2;
-int COLUMN_HEIGHT = 7;
+#define PIN 10
+int COLUMNS = 2;
+int ROWS = 7;
 
-int DATA_PIN = 10;
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(COLUMNS_ENABLED * COLUMN_HEIGHT, DATA_PIN, NEO_GRB + NEO_KHZ800);
-
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(ROWS * COLUMNS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//  colorWipe(strip.Color(255, 0, 0), 50); // Red
+//  colorWipe(strip.Color(0, 255, 0), 50); // Green
+//  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  double array[] = {0.5, 0.1};
+  setColumn(array);
+  strip.show();
+  delay(1000);
 
 }
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void setColumn(double array[]) {
+  for (int i = 0; i < sizeof(array); i++) {
+    int targetHeight = round(array[i] * ROWS);
+    for (int j = 0; j < ROWS; j++) {
+      int currentPixel = i * ROWS + j;
+      if (i % 2 == 0) {
+        //count up
+        if (currentPixel < i * ROWS + targetHeight) {
+          strip.setPixelColor(currentPixel, 255);
+        } else {
+          strip.setPixelColor(currentPixel, 0);
+        }
+      } else {
+        //count down
+        if (currentPixel % ROWS == ROWS - targetHeight) {
+          strip.setPixelColor(currentPixel, 255);
+        } else {
+          strip.setPixelColor(currentPixel, 0);
+        }
+      }
+    }
+  }
+}
+
