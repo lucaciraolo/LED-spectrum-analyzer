e63c4b226a077b27cbdb7c587815987aef8eb1ea
Luca Ciraolo <lucaciraolo@gmail.com>
2015-02-27 17:43:10 -0800

    Got all but first column working

--- Arduino_Sketch/Arduino_Sketch.ino
+++ Arduino_Sketch/Arduino_Sketch.ino
@@ -1,12 +1,18 @@
+
 #include <Adafruit_NeoPixel.h>
-#include <FHT.h>
 
 #define LOG_OUT 1 // use the log output function
-#define FHT_N 16 // set to 256 point fht
+#define FHT_N 256 // set to 256 point fht
+#include <FHT.h>
 
 #define PIN 10
-int COLUMNS = 5;
-int ROWS = 7;
+const int COLUMNS = 7;
+const int ROWS = 7;
+
+unsigned long count = 0;
+
+double maximums[COLUMNS] ;
+double outputArray[COLUMNS];
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(ROWS * COLUMNS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -23,20 +29,7 @@ void loop() {
 //  colorWipe(strip.Color(255, 0, 0), 50); // Red
 //  colorWipe(strip.Color(0, 255, 0), 50); // Green
 //  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  int val = (FHT_N - 2) / 2;
-  double array[];
-  for (int = 2; i < FHT_N; i++) {
-    int total = 0;
-    
-  }
-  
-  setColumn(array);
-  strip.show();
-  delay(1000);
 
-}
-
-void runFHT() {
   for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
       while(!(ADCSRA & 0x10)); // wait for adc to be ready
       ADCSRA = 0xf5; // restart adc
@@ -52,10 +45,71 @@ void runFHT() {
   fht_run(); // process the data in the fht
   fht_mag_log(); // take the output of the fht
   sei();
+  
+//  Serial.println("start");
+//    for (byte i = 0 ; i < FHT_N/2 ; i++) {
+//      Serial.println(fht_log_out[i]); // send out the data
+//    }
+    
+  
+  int val = (FHT_N / 2 - 2) / COLUMNS;
+  double total = 0;
+  for (int i = 2; i < FHT_N / 2; i++) {
+//      Serial.println(total);
+
+    if ((i - 2) % val != 0) {
+//      Serial.print(i);
+//      Serial.print(" ");
+//      Serial.println(fht_log_out[i]);
+      total =+ fht_log_out[i];
+    } else {
+      int index = (i - 2) / val;
+//      Serial.print(index);
+//      Serial.print(" ");
+
+      double newAverage = total / val;
+      
+//      Serial.println(total);
+      
+      
+      if (newAverage > maximums[index]) {
+        maximums[index] = newAverage;
+      }
+//      Serial.println(newAverage);
+      if (index == 0) {
+//        Serial.println(total);
+      }
+      outputArray[index] = newAverage / maximums[index];
+//              Serial.println(maximums[index]);
+      total = 0;
+    }
+  }
+
+//  for (int i = 0; i < COLUMNS; i++) {
+//    outputArray[i] = 1;
+//  }
+//  Serial.println(millis());
+  
+//  Serial.write(255); // send a start byte
+//  Serial.write(outputArray, (FHT_N/2));
+  
+//    Serial.write(fht_log_out, (FHT_N/2)); // send out the data
+    
+  if (millis() - count > 50){
+    setColumn();
+    strip.show();
+    count = millis();
+  }
+
+  
+//  delay(50);
+
 }
+
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
+//        strip.setPixelColor(0, 50);
   for(uint16_t i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, c);
       strip.show();
@@ -63,23 +117,36 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void setColumn(double array[]) {
+void setColumn() {
+
   for (int i = 0; i < COLUMNS; i++) {
-    int targetHeight = round(array[i] * ROWS);
+    int targetHeight = round(outputArray[i] * ROWS);
+//    Serial.print("column ");
+//    Serial.print(i);
+//    Serial.print(" = ");
+//    Serial.println(targetHeight);
     for (int j = 0; j < ROWS; j++) {
       int currentPixel = i * ROWS + j;
-//      Serial.println(currentPixel);
       if (i % 2 == 0) {
         //count up
-        if (currentPixel < i * ROWS + targetHeight) {
-          strip.setPixelColor(currentPixel, 255);
+        if (currentPixel < (i * ROWS) + targetHeight) {
+          int r = 255;
+          int g = 0;
+          int b = 0;
+            
+            
+          strip.setPixelColor(currentPixel, strip.Color(g, r, b));
         } else {
           strip.setPixelColor(currentPixel, 0);
         }
       } else {
         //count down
         if (currentPixel % ROWS >= ROWS - targetHeight) {
-          strip.setPixelColor(currentPixel, 255);
+          int r = 255;
+          int g = 0;
+          int b = 0;
+          
+          strip.setPixelColor(currentPixel, strip.Color(g, r, b));
         } else {
           strip.setPixelColor(currentPixel, 0);
         }
